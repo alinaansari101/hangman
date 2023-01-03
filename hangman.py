3d0498b69b6fe4fd1f11a41a02e15c8a9a82d6ca
alinaansari101 <alinaansari101@outlook.com>
2023-01-03 21:52:53 +0500

    Formatted code

--- hangman.py
+++ hangman.py
@@ -2,7 +2,7 @@ import random
 from random_word import RandomWords
 
 
-stages = ['''
+STAGES = ['''
   +---+
   |   |
   O   |
@@ -59,52 +59,48 @@ stages = ['''
 =========
 ''']
 
+NUM_OF_TRIES = 6
 
 word_generator =  RandomWords()
 chosen_word = word_generator.get_random_word()
 
 display = []
-len = len(chosen_word)
-
-
+word_len = len(chosen_word)
 underscores = 0
 
-while underscores < len:
+while underscores < word_len:
   display.append("_")
   underscores += 1
 
-tries = 6
 
-while tries > 0:
+while NUM_OF_TRIES > 0:
   guess = input("Guess a letter: ").lower()
   is_match = False
 
   # check if character
-  for i in range(len):
+  for i in range(word_len):
     if chosen_word[i] == guess:
       display[i] = chosen_word[i]
       is_match = True
       
-      
-    
   # win condition
   if "".join(display) == chosen_word:
     print("You win")
-    break
-    print(display)  
+    print(display)
     print(f"The word was {chosen_word}, you guessed correctly!")
+    
 
   # wrong try
   if is_match == False:
-    tries -= 1
+    NUM_OF_TRIES -= 1
   
   else:
     print(display)
     
   # lose condition
-  if tries == 0:
+  if NUM_OF_TRIES == 0:
       print("You lose")
       print(f"The word was {chosen_word}, you guessed wrong!")
       break
 
-  print(stages[tries])
+  print(STAGES[NUM_OF_TRIES])
